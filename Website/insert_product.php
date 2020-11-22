0f9539348122356230a154fe2c326bdf2038ff51
shauf280 <35082490+shauf280@users.noreply.github.com>
2020-11-22 13:44:32 +0000

    New website folder for easier use (data originally handled with two folders )

---
+++ Website/insert_product.php
@@ -0,0 +1,278 @@
+<?php 
+
+include("includes/db.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Insert Products Bid </title>
+    <link rel="stylesheet" href="css/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+</head>
+<body>
+    
+<div class="row"><!-- row Begin -->
+    
+    <div class="col-lg-12"><!-- col-lg-12 Begin -->
+        
+        <ol class="breadcrumb"><!-- breadcrumb Begin -->
+            
+            <li class="active"><!-- active Begin -->
+                
+                <i class="fa fa-dashboard"></i> Dashboard / Insert Products
+                
+            </li><!-- active Finish -->
+            
+        </ol><!-- breadcrumb Finish -->
+        
+    </div><!-- col-lg-12 Finish -->
+    
+</div><!-- row Finish -->
+       
+<div class="row"><!-- row Begin -->
+    
+    <div class="col-lg-12"><!-- col-lg-12 Begin -->
+        
+        <div class="panel panel-default"><!-- panel panel-default Begin -->
+            
+           <div class="panel-heading"><!-- panel-heading Begin -->
+               
+               <h3 class="panel-title"><!-- panel-title Begin -->
+                   
+                   <i class="fa fa-money fa-fw"></i> Insert Bid Product 
+                   
+               </h3><!-- panel-title Finish -->
+               
+           </div> <!-- panel-heading Finish -->
+           
+           <div class="panel-body"><!-- panel-body Begin -->
+               
+               <form method="post" class="form-horizontal" enctype="multipart/form-data"><!-- form-horizontal Begin -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label">Bid Product Title </label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <input name="bid_product_title" type="text" class="form-control" required>
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label"> Bid Product Category </label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <select name="bid_product_cat" class="form-control"><!-- form-control Begin -->
+                              
+                              <option> Select a Bid Category Product </option>
+                              
+                              <?php 
+                              
+                              $get_p_cats = "select * from bid_product_categories";
+                              $run_p_cats = mysqli_query($con,$get_p_cats);
+                              
+                              while ($row_p_cats=mysqli_fetch_array($run_p_cats)){
+                                  
+                                  $bid_p_cat_id = $row_p_cats['bid_p_cat_id'];
+                                  $bid_p_cat_title = $row_p_cats['bid_p_cat_title'];
+                                  
+                                  echo "
+                                  
+                                  <option value='$bid_p_cat_id'> $bid_p_cat_title </option>
+                                  
+                                  ";
+                                  
+                              }
+                              
+                              ?>
+                              
+                          </select><!-- form-control Finish -->
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label"> Category </label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <select name="bid_cat" class="form-control"><!-- form-control Begin -->
+                              
+                              <option> Select a Category </option>
+                              
+                              <?php 
+                              
+                              $get_cat = "select * from bid_categories";
+                              $run_cat = mysqli_query($con,$get_cat);
+                              
+                              while ($row_cat=mysqli_fetch_array($run_cat)){
+                                  
+                                  $bid_cat_id = $row_cat['bid_cat_id'];
+                                  $bid_cat_title = $row_cat['bid_cat_title'];
+                                  
+                                  echo "
+                                  
+                                  <option value='$bid_cat_id'> $bid_cat_title </option>
+                                  
+                                  ";
+                                  
+                              }
+                              
+                              ?>
+                              
+                          </select><!-- form-control Finish -->
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label">Bid Product Image 1 </label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <input name="bid_product_img1" type="file" class="form-control" required>
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label">Bid Product Image 2 </label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <input name="bid_product_img2" type="file" class="form-control">
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label">Bid Product Image 3 </label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <input name="bid_product_img3" type="file" class="form-control form-height-custom">
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label">Bid Product Price </label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <input name="bid_product_price" type="text" class="form-control" required>
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label">Bid Product Keywords </label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <input name="bid_product_keywords" type="text" class="form-control" required>
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label">Bid Product Desc </label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <textarea name="bid_product_desc" cols="19" rows="6" class="form-control"></textarea>
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label"></label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <input name="submit" value="Insert Product Bid" type="submit" class="btn btn-primary form-control">
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+               </form><!-- form-horizontal Finish -->
+               
+           </div><!-- panel-body Finish -->
+            
+        </div><!-- canel panel-default Finish -->
+        
+    </div><!-- col-lg-12 Finish -->
+    
+</div><!-- row Finish -->
+        
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script> 
+    <script src="js/tinymce/tinymce.min.js"></script>
+    <script>tinymce.init({ selector:'textarea'});</script>
+</body>
+</html>
+
+
+<?php 
+
+if(isset($_POST['submit'])){
+    
+    $bid_product_title = $_POST['bid_product_title'];
+    $bid_product_cat = $_POST['bid_product_cat'];
+    $bid_cat = $_POST['bid_cat'];
+    $bid_product_price = $_POST['bid_product_price'];
+    $bid_product_keywords = $_POST['bid_product_keywords'];
+    $bid_product_desc = $_POST['bid_product_desc'];
+    
+    $bid_product_img1 = $_FILES['bid_product_img1']['name'];
+    $bid_product_img2 = $_FILES['bid_product_img2']['name'];
+    $bid_product_img3 = $_FILES['bid_product_img3']['name'];
+    
+    $temp_name1 = $_FILES['bid_product_img1']['tmp_name'];
+    $temp_name2 = $_FILES['bid_product_img2']['tmp_name'];
+    $temp_name3 = $_FILES['bid_product_img3']['tmp_name'];
+    
+    move_uploaded_file($temp_name1,"product_images/$bid_product_img1");
+    move_uploaded_file($temp_name2,"product_images/$bid_product_img2");
+    move_uploaded_file($temp_name3,"product_images/$bid_product_img3");
+    
+    $insert_product = "insert into bid_products (bid_p_cat_id,bid_cat_id,date,bid_product_title,bid_product_img1,bid_product_img2,bid_product_img3,bid_product_price,bid_product_keywords,product_desc) values ('$bid_product_cat','$bid_cat',NOW(),'$bid_product_title','$bid_product_img1','$bid_product_img2','$bid_product_img3','$bid_product_price','$bid_product_keywords','$bid_product_desc')";
+    
+    $run_product = mysqli_query($con,$insert_product);
+    
+    if($run_product){
+        
+        echo "<script>alert('Bid Product has been inserted sucessfully')</script>";
+        echo "<script>window.open('insert_product.php','_self')</script>";
+        
+    }
+    
+}
+
+?>
